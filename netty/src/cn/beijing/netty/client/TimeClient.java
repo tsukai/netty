771cd1a0ec7b312fb941f717ca6d7dc7db7c41b7
zukai <tsukai2012@163.com>
2015-11-24 09:29:53 +0800

    bio

---
+++ netty/src/cn/beijing/netty/client/TimeClient.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package cn.beijing.netty.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * @author zukai
+ *
+ */
+public class TimeClient {
+	public static void main(String[] args) {
+		int port = 7070;
+		if (args != null && args.length > 0) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				
+			}
+		}
+		Socket socket = null;
+		BufferedReader in = null;
+		PrintWriter out = null;
+		try {
+			socket = new Socket("127.0.0.1", port);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(),true);
+			out.println("QUERY TIME ORDER");
+			System.out.println("Send order 2 server succeed");
+			String resp = in.readLine();
+			System.out.println("now is : "+resp);
+		} catch (Exception e) {
+			
+		}finally{
+			if(in != null){
+				try {
+					in.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				in = null;
+			}
+			if(out != null){
+				out.close();
+				out = null;
+			}
+			if(socket != null){
+				try {
+					socket.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				socket = null;
+			}
+		}
+	}
+}
---
+++ netty/src/cn/beijing/netty/server/TimeServer.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package cn.beijing.netty.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import cn.beijing.netty.handler.TimeServerHandler;
+
+/**
+ * @author zukai
+ * 
+ */
+public class TimeServer {
+	public static void main(String[] args) throws IOException {
+		int port = 7070;
+		if (args != null && args.length > 0) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				
+			}
+		}
+		ServerSocket server = null;
+		try{
+			server = new ServerSocket(port);
+			System.out.println("The time server is start in port : "+port);
+			Socket socket = null;
+			while(true){
+				socket = server.accept();
+				new Thread(new TimeServerHandler(socket)).start();
+			}
+		}finally{
+			if(server != null){
+				System.out.println("The time server close");
+				server.close();
+				server = null;
+			}
+		}
+	}
+}
