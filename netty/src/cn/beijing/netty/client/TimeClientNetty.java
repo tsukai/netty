4c2816f43ec06bbf654086225c80f4fe1d2192f9
zukai <tsukai2012@163.com>
2015-11-25 11:41:57 +0800

    netty

---
+++ netty/src/cn/beijing/netty/client/TimeClientNetty.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package cn.beijing.netty.client;
+
+import cn.beijing.netty.handler.TimeClientNettyHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author zukai 2015-11-25
+ */
+public class TimeClientNetty {
+	public static void main(String[] args) throws Exception{
+		int port = 7070;
+		if (args != null && args.length > 0) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				
+			}
+		}
+		new TimeClientNetty().connect(port,"127.0.0.1");
+	}
+
+	private void connect(int port, String host) throws Exception{
+		//配置客户端Nio线程组
+		EventLoopGroup group = new NioEventLoopGroup();
+		try{
+			Bootstrap b = new Bootstrap();
+			b.group(group).channel(NioSocketChannel.class)
+				.option(ChannelOption.TCP_NODELAY, true)
+				.handler(new ChannelInitializer<SocketChannel>() {
+
+					@Override
+					protected void initChannel(SocketChannel ch)
+							throws Exception {
+						ch.pipeline().addLast(new TimeClientNettyHandler());
+					}
+					
+				});
+			//发起异步连接操作
+			ChannelFuture f = b.connect(host,port).sync();
+			//等待客户端链路关闭
+			f.channel().closeFuture().sync();
+		}finally{
+			group.shutdownGracefully();
+		}
+	}
+}
---
+++ netty/src/cn/beijing/netty/server/TimeServerNetty.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package cn.beijing.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import cn.beijing.netty.handler.TimeServerNettyHandler;
+
+/**
+ * @author zukai 2015-11-25
+ */
+public class TimeServerNetty {
+	public static void main(String[] args) throws Exception {
+		int port = 7070;
+		if (args != null && args.length > 0) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				
+			}
+		}
+		new TimeServerNetty().bind(port);
+	}
+
+	private void bind(int port) throws Exception {
+		//配置服务端的NIO线程组
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try{
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup,workerGroup)
+				.channel(NioServerSocketChannel.class)
+				.option(ChannelOption.SO_BACKLOG, 1024)
+				.childHandler(new ChildChannelHandler());
+			//绑定端口，同步等待成功
+			ChannelFuture f = b.bind(port).sync();
+			//等待服务端监听端口关闭
+			f.channel().closeFuture().sync();
+		}finally{
+			//释放线程池资源
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
+	
+	private class ChildChannelHandler extends ChannelInitializer<io.netty.channel.socket.SocketChannel>{
+
+		@Override
+		protected void initChannel(io.netty.channel.socket.SocketChannel arg0)
+				throws Exception {
+			arg0.pipeline().addLast(new TimeServerNettyHandler());
+		}
+
+		
+	}
+}
